c7d0c286b0f34b348e030d9fcc8c5587c02d54f0
ailnytskyy <ailnytskyy@griddynamics.com>
2019-10-19 00:01:52 -0700

    Add Maximum Size Subarray Solution

---
+++ array/Minimum_Size_Subarray_Sum_209/Main.java
@@ -0,0 +1,11 @@
+package Minimum_Size_Subarray_Sum_209;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {2,3,1,2,4,3};
+        int target = 7;
+        int result = s.minSubArrayLen(target, arr);
+        System.out.print(result);
+    }
+}
