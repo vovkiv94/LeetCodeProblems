2f3852296162cc1a9b8dfbf85309c5fb7443b402
ailnytskyy <ailnytskyy@griddynamics.com>
2019-10-19 00:02:06 -0700

    Add Next Permutation Solution

---
+++ array/Next_Permutation_31/Solution.java
@@ -0,0 +1,49 @@
+package Next_Permutation_31;
+
+
+/**
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * <p>
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * <p>
+ * The replacement must be in-place and use only constant extra memory.
+ * <p>
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * <p>
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * <p>
+ * Accepted
+ */
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
